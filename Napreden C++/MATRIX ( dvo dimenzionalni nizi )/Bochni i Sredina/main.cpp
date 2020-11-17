a9a4e5d225c3db6f571a09dc5e18e766289fa144
filipmazev <filipmazev@users.noreply.github.com>
2020-11-17 22:37:28 +0100

    c
    2

--- Napreden C++/MATRIX ( dvo dimenzionalni nizi )/Bochni i Sredina/main.cpp
+++ Napreden C++/MATRIX ( dvo dimenzionalni nizi )/Bochni i Sredina/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n; cin>>n; int A[n][n];
+    int n; cin>>n; int A[n][m];
     for ( int i=0; i<n; i++) {
           for ( int j=0; j<n; j++) {
             A[i][j] = 1; } }
