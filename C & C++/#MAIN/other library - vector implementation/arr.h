4bb7e22f507c75bc34d53478edff085be0ba348d
filipmazev <fmazev@yahoo.com>
2022-03-29 01:25:05 +0200

    other library - dynamic array implementation - version 1.0.4
    This is a minimal implementation for dynamically allocated memory for arrays of variable type within the standard C++ GCC library
    
    Written in 2022 by Filip Mazev (@mazevmazev)
    
    To the extent possible under law, the author(s) have dedicated all
    copyright and related and neighboring rights to this software to the
    public domain worldwide. This software is distributed without any warranty.
    
    CC0 Public Domain Dedication <http://creativecommons.org/publicdomain/zero/1.0/>.
    
    A minimal implementation of dynamic arrays / std::oth::arr

---
+++ C & C++/#MAIN/other library - vector implementation/arr.h
@@ -0,0 +1,131 @@
+///
+// Written in 2022 by Filip Mazev (@mazevmazev)
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to the
+// public domain worldwide. This software is distributed without any warranty.
+//
+// CC0 Public Domain Dedication <http://creativecommons.org/publicdomain/zero/1.0/>.
+// A minimal implementation of dynamic arrays / oth::arr
+///
+
+/** @file other/arr */
+
+#ifndef _GLIBCXX_EXPERIMENTAL_SET
+#define _GLIBCXX_EXPERIMENTAL_SET 1
+
+#pragma GCC system_header
+
+#if __cplusplus >= 201402L
+
+#include <iostream>
+#include <array>
+#include <list>
+
+namespace std _GLIBCXX_VISIBILITY(default)
+{
+    _GLIBCXX_BEGIN_NAMESPACE_VERSION
+    namespace oth
+    {
+        template <class TYPE> class arr
+        {
+        private:
+            size_t num{0}, _size{0}; TYPE *_arr{nullptr};
+        public:
+            arr() { this->num = 0; this->_size = 0; this->_arr = nullptr; }
+            ~arr() { delete[] this->_arr; this->_arr = nullptr; }
+
+            arr(const arr &other)
+            {
+                if (this != &other) {
+                    this->num = other.num; this->_size = other._size; delete[] this->_arr; auto *tmp = new TYPE[other.num];
+                    for (unsigned int i=0; i<this->num; i++) { tmp[i] = other._arr[i]; } this->_arr = tmp;
+                }
+            }
+
+            arr &operator = (arr &other)
+            {
+                if (this != &other) {
+                    this->num = other.num; this->_size = other._size; delete[] this->_arr; auto *tmp = new TYPE[other.num];
+                    for (unsigned int i=0; i<this->num; i++) { tmp[i] = other._arr[i]; } this->_arr = tmp;
+                }
+                return *this;
+            }
+
+            auto &operator[] (unsigned int index) { return this->_arr[index]; }
+
+            friend std::ostream &operator << (std::ostream &output, arr &other) {
+                for (unsigned int i=0; i<other.num; i++) { output << other._arr[i] << " "; } return output;
+            }
+
+            friend std::istream& operator >> (std::istream &input, arr &other)
+            {
+                other.num+1>other._size?++other._size:other._size; auto *tmp = new TYPE[other._size];
+                for (unsigned int i=0; i<other.num; i++) { tmp[i] = other._arr[i]; }
+                input >> tmp[other.num++]; delete[] other._arr; other._arr = tmp; return input;
+            }
+
+            inline void push_back(auto element)
+            {
+                this->num+1>this->_size?++this->_size:this->_size; auto *tmp = new TYPE[this->_size];
+                for (unsigned int i=0; i<this->num; i++) { tmp[i] = this->_arr[i]; }
+                tmp[this->num++] = element; delete[] this->_arr; this->_arr = tmp;
+            }
+
+            inline void set(unsigned int index, auto element) { if(index <= this->_size) this->_arr[index] = element; else { throw std::bad_alloc{}; } }
+
+            inline void pop_back()
+            {
+                if (!empty()) {
+                    --this->_size; --this->num; auto* tmp = new TYPE[this->_size];
+                    for (unsigned int i=0; i<this->num; i++) { tmp[i] = this->_arr[i]; }
+                    delete[] this->_arr; this->_arr = tmp;
+                }
+            }
+
+            inline void erase(int pos)
+            {
+                if (pos < this->num) {
+                    for (unsigned int i=0; i<this->num; i++) {
+                        if (i == pos) { --this->_size; --this->num;
+                            for (unsigned int j=i; j<this->num; j++) { this->_arr[j] = this->_arr[j + 1]; } break; } } }
+            }
+
+            template <typename... T> constexpr auto assign(T&&... t)->std::array <TYPE, sizeof...(T)>
+            {
+                std::list<TYPE> list = { { std::forward<T>(t)... } };
+                     this->num = list.size(); this->_size = list.size(); delete[] this->_arr; this->_arr = new TYPE[this->_size];
+                int k = 0; for (TYPE const& i : list) { this->_arr[k++] = i; }
+            }
+
+            template <typename... T> constexpr auto insert(T&&... t)->std::array <TYPE, sizeof...(T)>
+            {
+                std::list<TYPE> list = { { std::forward<T>(t)... } };
+                this->_size += list.size(); TYPE *tmp = new TYPE[this->_size]; for(int i=0; i<this->num; i++){ tmp[i] = this->_arr[i]; }
+                    int k = this->num; for (TYPE const& i : list) { tmp[k++] = i; } this->num += list.size();
+                delete[] this->_arr; this->_arr = new TYPE[this->_size]; this->_arr = tmp;
+            }
+
+            inline void resize(size_t new_size)
+            {
+                this->_size = new_size; TYPE *tmp = new TYPE[this->_size]; this->num = this->_size<this->num?this->_size:this->num;
+                    for(int i=0; i<this->num; i++){ tmp[i] = this->_arr[i]; }
+                delete[] this->_arr; this->_arr = new TYPE[this->_size]; this->_arr = tmp;
+            }
+
+            void print() { for (unsigned int i=0; i<this->num; i++) { std::cout<<_arr[i]<<" "; } }
+
+            inline void clear(){ while (!empty()) { pop_back(); } }
+
+            auto at(size_t index) noexcept { return this->_arr[index]; }
+            const size_t size() noexcept { return this->_size; }
+            const bool empty() noexcept { return (this->num == 0); }
+            auto back() noexcept { return this->_arr[this->num - 1]; }
+            auto front() noexcept { return this->_arr[0]; }
+        };
+    } // namespace oth
+} // namespace std
+
+#endif // C++14
+
+#endif // _GLIBCXX_EXPERIMENTAL_SET
--- C & C++/#MAIN/other library - vector implementation/sample/main.cpp
+++ C & C++/#MAIN/other library - vector implementation/sample/main.cpp
@@ -1,9 +1,108 @@
 #include <iostream>
-#include "other/vector.h"
+#include <string> //not required, for to_string function in Class_Example
+#include "other/arr.h" //if linked: #include <other/arr.h>
+
+class Class_Example
+{
+private:
+    int n{0}; std::string str;
+public:
+    Class_Example(){ this->n=0; }
+    Class_Example(int n, std::string str){ this->n=n; this->str = str; this->str += std::to_string(n+1); }
+
+    friend std::ostream &operator << (std::ostream &out, Class_Example &other){ out<<" "<<other.n<<" "<<other.str<<" |"; return out; }
+
+    ~Class_Example(){}
+};
 
 int main()
 {
-    std::oth::vector<std::string> my_vector;
-    int n; std::cin>>n; for(int i=0; i<n; i++){ std::cin>>my_vector; }
-    std::cout<<my_vector;
+    //initialization syntax: std::oth::arr<typename> name_of_arr; | accepts all types
+
+    /*
+    std::oth::arr<int> my_arr_of_ints; //initializes an array of integers with size 0 set as a NULL pointer | int *arr = nullptr;
+    std::oth::arr<float> my_arr_of_floats; //initializes an array of floats with size 0 set as a NULL pointer | floats *arr = nullptr;
+    std::oth::arr<double> my_arr_of_doubles; //initializes an array of doubles with size 0 set as a NULL pointer | double *arr = nullptr;
+    std::oth::arr<char> my_arr_of_chars; //initializes an array of chars with size 0 set as a NULL pointer | char *arr = nullptr;
+    std::oth::arr<std::string> my_arr_of_strings; //initializes an strings of integers with size 0 set as a NULL pointer | string *arr = nullptr;
+    std::oth::arr<Class_Example> my_arr_of_objects; //initializes an array of objects from a certain class with size 0 set as a NULL pointer | Class_Example *arr = nullptr;
+    */
+
+    /**
+    operator =  | copy (1) | arr &operator = (const arr &x); | The copy assignment (1) copies all the elements from x into the container (with x preserving its contents).
+    operator [] | auto operator[] (size_type n); | Returns the element at position n in the arr container.
+    operator << | friend std::ostream &operator << (std::ostream &input, const arr &x); | prints every element in array from range array.begin() to array.end()
+    operator >> | friend std::istream &operator >> (std::istream &input, const arr &x); | input<<arr[size++]; return input; | enter a element in array, expands the array for size of n+1
+    push_back() | push_back an element in array, expands the array for size of n+1
+    set()       | set(size_t index, element) | sets the value of the array element at index (size_t index) to the sent element if in range, otherwise throws bad::alloc
+    assign()    | syntax: my_array.assign(element1, element2, element3 ... ) | changes the size and values of the current array to whatever is assigned | *works only for int, char, double, float, size_t, bool and their unsigned or long/long long counterparts
+    insert()    | syntax: my_array.insert(element1, element2, element3 ... ) | appends the values in (...) to the end of the array and changes its size accordingly | *works only for int, char, double, float, size_t, bool and their unsigned or long/long long counterparts
+    pop_back()  | pop back the array for one element, reduces the size of the array for n-=1
+    clear()     | deletes every element in the array and sets the size to 0
+    erase()    | remove(size_t i) remove a specific element in array at index size_t i
+    front()     | returns the first element of the array, element at position 0
+    back()      | return the last element of the array, element at position size-1
+    size()      | returns the current size of the array
+    resize()    | change the size of the array
+    */
+
+    // EXAMPLE CODE
+    std::oth::arr<Class_Example> my_arr_of_objects; int n; std::cout<<std::endl<<" ENTER SIZE: "; std::cin>>n; std::cout<<std::endl;
+
+    for(int i=0; i<n; i++){ Class_Example obj = Class_Example(i, "test"); my_arr_of_objects.push_back(obj); }
+
+    //operator >>
+    std::cout<<" operator >> \t \t |"<<my_arr_of_objects<<std::endl;
+
+    //operator []
+    std::cout<<" operator [] \t \t |"<<my_arr_of_objects[n-1]<<std::endl;
+
+    //erase()
+    std::cout<<" function erase() \t |"; my_arr_of_objects.erase(n-1); std::cout<<my_arr_of_objects<<std::endl;
+
+    //pop_back()
+    std::cout<<" function pop_back() \t |"; my_arr_of_objects.pop_back(); std::cout<<my_arr_of_objects<<std::endl;
+
+    //size()
+    std::cout<<" function size() \t | size: "<<my_arr_of_objects.size()<<std::endl;
+
+    //resize()
+    std::cout<<" function resize() \t | new size: "; my_arr_of_objects.resize(n/2); std::cout<<my_arr_of_objects.size()<<std::endl;
+
+    //print()
+    std::cout<<" function print() \t |"; my_arr_of_objects.print(); std::cout<<std::endl;
+
+    //push_back()
+    Class_Example obj2 = Class_Example(0, "add1");
+    std::cout<<" function push_back() \t |"; my_arr_of_objects.push_back(obj2); my_arr_of_objects.print(); std::cout<<std::endl;
+
+    //set()
+    Class_Example obj_set = Class_Example(1, "set");
+    std::cout<<" function set() \t |"; my_arr_of_objects.set(0, obj_set); my_arr_of_objects.print(); std::cout<<std::endl;
+
+    //clear()
+    std::cout<<" function clear() \t | size: "; my_arr_of_objects.clear(); std::cout<<my_arr_of_objects.size(); my_arr_of_objects.print(); std::cout<<std::endl;
+
+    std::cout<<std::endl<<" DYNAMIC RESIZING AND OTHER FUNCTIONS: "<<std::endl; std::oth::arr<int> my_arr_of_ints; std::cout<<std::endl;
+
+    //assign
+    my_arr_of_ints.assign(1,2,3,4,5,6,7,8);
+    std::cout<<" function assign() \t | size: "; std::cout<<my_arr_of_ints.size()<<" | "; my_arr_of_ints.print(); std::cout<<std::endl;
+
+    //resize
+    my_arr_of_ints.resize(5);
+    std::cout<<" function resize() \t | size: "; std::cout<<my_arr_of_ints.size()<<" | "; my_arr_of_ints.print(); std::cout<<std::endl;
+
+    //pop_back
+    my_arr_of_ints.pop_back();
+    std::cout<<" function pop_back() \t | size: "; std::cout<<my_arr_of_ints.size()<<" | "; my_arr_of_ints.print(); std::cout<<std::endl;
+
+    //insert
+    my_arr_of_ints.insert(9,10,11,12);
+    std::cout<<" function insert() \t | size: "; std::cout<<my_arr_of_ints.size()<<" | "; my_arr_of_ints.print(); std::cout<<std::endl;
+
+    //clear
+    std::cout<<" function clear() \t | size: ";  my_arr_of_ints.clear(); std::cout<<my_arr_of_ints.size()<<std::endl;
+
+    return 0;
 }
